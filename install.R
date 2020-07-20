202e898f73377d0bba5b55824b00bec2d2806316
Jigyasa Arora <arorajigyasa1992@gmail.com>
2020-07-20 20:18:54 +0900

    Create install.R

---
+++ install.R
@@ -0,0 +1,21 @@
+install.packages("BiocManager")
+
+BiocManager::install("Rbowtie2")
+BiocManager::install("Rsamtools")
+BiocManager::install("BBCAnalyzer")
+
+
+install.packages("dplyr")
+
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("VariantTools") #OR
+BiocManager::install("VariantTools")
+
+install.packages("devtools")
+
+devtools::install_github(repo="knausb/vcfR") 
+#For Mac- make sure "checkbashisms" is installed <https://brewinstall.org/install-checkbashisms-on-mac-with-brew/>
+#For Windows- make sure "Rtools" is installed <https://cran.r-project.org/bin/windows/Rtools/>
+
+install.packages("ape")
+
